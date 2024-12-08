be86e6167c999e95cf5161ddc1f960f6162d1fe7
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-12-08 08:58:59 +0000

    Initial commit

---
+++ src/packages.sh
@@ -0,0 +1,28 @@
+DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+apt-get upgrade
+apt-get install \
+  git \
+  jq \
+  htop \
+  wget
+
+# Yq requires manual retrieval of the package
+echo "Installing yq:$YQ_VERSION…"
+wget \
+  https://github.com/mikefarah/yq/releases/download/$YQ_VERSION/yq_linux_amd64 \
+  -O /usr/bin/yq && \
+  chmod +x /usr/bin/yq
+
+# Neovim requires manual retrieval of the latest version
+# as the apt package is quite old
+echo "Installing neovim:$NVIM_VERSION…"
+wget https://github.com/neovim/neovim/releases/download/$NVIM_VERSION/nvim-linux64.deb \
+  -O /neovim.deb
+apt install -y /neovim.deb 
+rm /neovim.deb
+
+# VScode folders that need to exist
+mkdir -p $HOME/.vscode-server/extensions
+mkdir -p $HOME/.vscode-server-insiders/extensions
---
+++ src/ubuntu.dockerfile
@@ -0,0 +1,19 @@
+ARG OS_TAG
+ARG YQ_VERSION
+ARG NVIM_VERSION
+ARG USERNAME
+ARG GROUP
+ARG ROOT_PASS
+
+FROM ubuntu:$OS_TAG
+RUN echo "root:$ROOT_PASS" | chpasswd
+
+COPY packages.sh /bootstrap/
+RUN /bootstrap/packages.sh
+
+USER $USERNAME
+
+COPY gists.sh /bootstrap/
+RUN /bootstrap/gists.sh
+
+ENV EDITOR=nvim
