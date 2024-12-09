e41e1bd168b68f8130b41e4c2a8070d71a71db37
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-12-09 09:59:53 +0000

    Fix

--- src/scripts/bootstrap/users.sh
+++ src/scripts/bootstrap/users.sh
@@ -2,11 +2,21 @@
 set -eux
 
 root_pass=${1:?'Root pass required as param #1'}
-username=${1:?'Username required as param #2'}
+username=${2:?'Username required as param #2'}
+user_id=${3:?'User ID required as param #3'}
+group_id=${4:?'Group ID required as param #4'}
 default_user=ubuntu
+home_path="/home/${username}"
 
 echo "root:$root_pass" | chpasswd
 
-mv /home/${default_user} /home/${username}
-usermod -l ${username} ${default_user}
+userdel -r ${default_user}
+useradd -m -u ${user_id} -s -g ${group_id} /bin/bash ${username}
+mkdir -p ${home_path}
+chown -R ${user_id}:${group_id} ${home_path}
+
+# mv /home/${default_user} /home/${username}
+# usermod -l ${username} ${default_user}
+
+ls -al /home
 # usermod -d /home/${username} -m ${username}
