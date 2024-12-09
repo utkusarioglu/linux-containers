a2f60b09efff2d03fb4158d183522ca4bf0c32b2
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-12-09 13:28:12 +0000

    Fix

--- src/scripts/bootstrap/gists.sh
+++ src/scripts/bootstrap/gists.sh
@@ -13,13 +13,14 @@ scripts_abspath=${home_abspath}/scripts
 
 mkdir ${scripts_abspath}
 
-# Create env example
-wget -O ${scripts_abspath}/create-env-example.sh ${base}/2d4be44dc7707afccd540ad99ba385e6/raw/create-env-example.sh
-chmod +x ${scripts_abspath}/create-env-example.sh
+# # Create env example
+# wget -O ${scripts_abspath}/create-env-example.sh \
+#   ${base}/2d4be44dc7707afccd540ad99ba385e6/raw/create-env-example.sh
+# chmod +x ${scripts_abspath}/create-env-example.sh
 
-# RC files
-wget -O ${home_abspath}/.bashrc ${base}/3523b00578807d63b05399fe57a4b2a7/raw/.bashrc
-wget -O ${home_abspath}/.inputrc ${base}/d5c216c744460c45bf6260d0de4131b4/raw/.inputrc
+# # RC files
+# wget -O ${home_abspath}/.bashrc ${base}/3523b00578807d63b05399fe57a4b2a7/raw/.bashrc
+# wget -O ${home_abspath}/.inputrc ${base}/d5c216c744460c45bf6260d0de4131b4/raw/.inputrc
 
 # Elam
 elam_path=${home_abspath}/elam
--- src/scripts/bootstrap/packages.sh
+++ src/scripts/bootstrap/packages.sh
@@ -32,10 +32,10 @@ done
 
 # Yq requires manual retrieval of the package
 echo "Installing yq:$yq_version…"
-wget \
-  https://github.com/mikefarah/yq/releases/download/$yq_version/yq_linux_amd64 \
-  -O /usr/bin/yq && \
-  chmod +x /usr/bin/yq
+wget https://github.com/mikefarah/yq/releases/download/$yq_version/yq_linux_amd64 \
+  -O /usr/bin/yq
+chmod +x /usr/bin/yq
+yq --version
 
 # Neovim requires manual retrieval of the latest version
 # as the apt package is quite old
@@ -50,9 +50,10 @@ mv ./nvim-linux64/bin/nvim $nvim_path/nvim
 rm -rf nvim_gz_path ./nvim-linux64
 echo 'Following path should be clear of nvim files:'
 ls -al
-echo 'Nvim version:'
 nvim --version
 
 # VScode folders that need to exist
 mkdir -p ${home_abspath}/.vscode-server/extensions
 mkdir -p ${home_abspath}/.vscode-server-insiders/extensions
+
+ls -al ${home_abspath}
