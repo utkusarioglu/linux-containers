b8938df809fc27fd445707b3318726e8cdbbaec9
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-12-10 11:33:05 +0000

    Fix

--- src/home/scripts/bootstrap/create-folders.sh
+++ src/home/scripts/bootstrap/create-folders.sh
@@ -19,7 +19,7 @@ ARGS=(
 for suffix in "" "-insiders"; do
   abspath=${home_abspath}/.vscode-server${suffix}/extensions
   mkdir -p $abspath
-  chown ${user_id}:${group_id} $abspath
+  chown -R ${user_id}:${group_id} $abspath
 done
 
 ls -al ${home_abspath}
