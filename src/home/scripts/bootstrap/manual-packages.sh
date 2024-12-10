ed43374bcdb92edc7023977ff85b2283449342db
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-12-10 12:05:28 +0000

    Fix

--- src/home/scripts/bootstrap/manual-packages.sh
+++ src/home/scripts/bootstrap/manual-packages.sh
@@ -42,4 +42,4 @@ nvim --version
 elam_path=${home_abspath}/elam
 git clone --depth 1 https://github.com/utkusarioglu/elam.git $elam_path
 echo "alias elam=$elam_path/elam.sh" >> ${home_abspath}/.bash_aliases
-chown -R 
+chown -R ${elam_path}
