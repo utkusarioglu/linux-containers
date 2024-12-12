2cb9f08eb02e78bfb05fd0ceb57dee524a788376
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-12-12 16:15:12 +0000

    Fix

--- src/home/scripts/bootstrap/linux/install-apt.sh
+++ src/home/scripts/bootstrap/linux/install-apt.sh
@@ -7,20 +7,14 @@ set -eux
 bash --version
 
 no_check="false"
-allow_blank="false"
 
 if [ $1 == "--no-check" ]; then
   no_check="true"
   shift
 fi
 
-if [ $1 == "--allow-blank" ]; then
-  allow_blank="true"
-  shift
-fi
-
-if [ ${allow_blank} == "true" ] && [ $# -eq 0 ]; then
-  echo "Warning --allow-blank enabled with empty block, exiting"
+if [ ${1:-'none'} == 'none' ]; then
+  echo "Warning: First arg empty, exiting"
   exit 0
 fi
 
